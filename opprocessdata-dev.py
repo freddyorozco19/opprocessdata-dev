a4c0a67a509ebdfe4e2a98cec3be203e76b01a39
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-03-16 12:03:01 -0500

    Create opprocessdata-dev.py

---
+++ opprocessdata-dev.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 03:14:41 2023
+
+@author: Freddy J. Orozco R.
+@Powered: WinStats.
+"""
+
+
+import streamlit as st
+import hydralit_components as hc
+import datetime
+import base64
+import pandas as pd
+from io import BytesIO
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as mplt
+import matplotlib.font_manager as font_manager
+import mplsoccer
+from mplsoccer import Pitch, VerticalPitch, FontManager
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial import ConvexHull
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.patheffects as path_effects
+from scipy.ndimage import gaussian_filter
+import seaborn as sns
+from matplotlib import colors as mcolors
+import requests
+from PIL import Image
+from matplotlib.patches import Rectangle
+import math
+
+############################################################################################################################################################################################################################
+def split_minute(minute_str):
+    if '+' in minute_str:
+        parts = minute_str.split('+')
+        return parts[0], parts[1]
+    else:
+        return minute_str, ''
+
+def calcular_radio_desde_gol(x, y, y_gol):
+    radio = math.sqrt(x**2 + (y - y_gol)**2)
+    return radio
+def es_deep_completion(row):
+    radio = math.sqrt(row['X1']**2 + (row['Y1'] - y_gol_oponente)**2)
+    return radio <= radio_umbral
+def esta_dentro_semicircunferencia(x, y):
+    distancia_centro = math.sqrt((x - punto_medio_cancha_rival[0])**2 + (y - punto_medio_cancha_rival[1])**2)
+    return distancia_centro <= radio_umbral
+
+# Función para calcular la distancia euclidiana entre dos puntos
+def distancia_entre_puntos(x1, y1, x2, y2):
+    return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+def colorlist(color1, color2, num):
+    """Generate list of num colors blending from color1 to color2"""
+    result = [np.array(color1), np.array(color2)]
+    while len(result) < num:
+        temp = [result[0]]
+        for i in range(len(result)-1):
+            temp.append(np.sqrt((result[i]**2+result[i+1]**2)/2))
+            temp.append(result[i+1])
+        result = temp
+    indices = np.linspace(0, len(result)-1, num).round().astype(int)
+    return [result[i] for i in indices] 
+
+hex_list2 = ['#121214', '#D81149', '#FF0050']
+#hex_list = ['#121214', '#112F66', '#004DDD']B91845
+hex_list4 = ['#5A9212', '#70BD0C', '#83E604']
+#hex_list1 = ['#121214', '#854600', '#C36700']
+hex_list = ['#121214', '#545454', '#9F9F9F']
+hex_list1 = ['#121214', '#695E00', '#C7B200']
+#hex_list2 = ['#121214', '#112F66', '#004DDD']
+#hex_list = ['#121214', '#11834C', '#00D570']
+cmap = sns.cubehelix_palette(start=.25, rot=-.3, light=1, reverse=True, as_cmap=True)
+cmap2 = sns.diverging_palette(250, 344, as_cmap=True, center="dark")
+cmap3 = sns.color_palette("dark:#FF0046", as_cmap=True)
+
+
+def hex_to_rgb(value):
+    '''
+    Converts hex to rgb colours
+    value: string of 6 characters representing a hex colour.
+    Returns: list length 3 of RGB values'''
+    value = value.strip("#") # removes hash symbol if present
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+
+def rgb_to_dec(value):
+    '''
+    Converts rgb to decimal colours (i.e. divides each value by 256)
+    value: list (length 3) of RGB values
+    Returns: list (length 3) of decimal values'''
+    return [v/256 for v in value]
+
+
+def get_continuous_cmap(hex_list, float_list=None):
+    ''' creates and returns a color map that can be used in heat map figures.
+        If float_list is not provided, colour map graduates linearly between each color in hex_list.
+        If float_list is provided, each color in hex_list is mapped to the respective location in float_list. 
+        
+        Parameters
+        ----------
+        hex_list: list of hex code strings
+        float_list: list of floats between 0 and 1, same length as hex_list. Must start with 0 and end with 1.
+        
+        Returns
+        ----------
+        colour map'''
+    rgb_list = [rgb_to_dec(hex_to_rgb(i)) for i in hex_list]
+    if float_list:
+        pass
+    else:
+        float_list = list(np.linspace(0,1,len(rgb_list)))
+        
+    cdict = dict()
+    for num, col in enumerate(['red', 'green', 'blue']):
+        col_list = [[float_list[i], rgb_list[i][num], rgb_list[i][num]] for i in range(len(float_list))]
+        cdict[col] = col_list
+    cmp = mcolors.LinearSegmentedColormap('my_cmp', segmentdata=cdict, N=256)
+    return cmp
+
+#####################################################################################################################################################
+
+font_path = 'Resources/keymer-bold.otf'  # Your font path goes here
+font_manager.fontManager.addfont(font_path)
+prop2 = font_manager.FontProperties(fname=font_path)
+
+font_path2 = 'Resources/BasierCircle-Italic.ttf'  # Your font path goes here
+font_manager.fontManager.addfont(font_path2)
+prop3 = font_manager.FontProperties(fname=font_path2)
+
+#####################################################################################################################################################
+
+
+###########################################################################################################################################################################################################################
+############################################################################################################################################################################################################################
+############################################################################################################################################################################################################################
+
+#make it look nice from the start
+st.set_page_config(layout='wide')
+
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
+    }
+   .sidebar .sidebar-content {
+        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+    
+# specify the primary menu definition
+menu_data = [
+    {'id': "ExploreTeamData", 'label':"Explore Data"},
+    {'id': "ProMatchStats", 'label':"ProMatchStats"}
+    #{'id': "Dashboard", 'icon': "fas fa-tachometer-alt", 'label':"Dashboard",'ttip':"I'm the Dashboard tooltip!"} #can add a tooltip message]
+]
+over_theme = {'txc_inactive': '#FFFFFF'}
+menu_id = hc.nav_bar(
+    menu_definition=menu_data,
+    override_theme=over_theme,
+    #login_name='Logout',
+    hide_streamlit_markers=True, #will show the st hamburger as well as the navbar now!
+    sticky_nav=True, #at the top or not
+    sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
+)
+
+############################################################################################################################################################################################################################
+############################################################################################################################################################################################################################
+############################################################################################################################################################################################################################
+
+if menu_id == "ExploreTeamData":
+    with st.sidebar:
+        with open("Resources/win.png", "rb") as f:
+            data = base64.b64encode(f.read()).decode("utf-8")
+        
+            st.sidebar.markdown(
+                f"""
+                <div style="display:table;margin-top:-20%">
+                    <img src="data:image/png;base64,{data}" width="300">
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+        
+        st.markdown("""---""")    
+        
+        with st.form(key='form'):
+            plt = st.text_area('Paste your source code')
+
+            TableName = st.text_input("Namefile",
+                                      key="filename")   
+            rsid01, rsid02 = st.columns(2)
+            with rsid01:
+                MatchID = st.text_input("MatchID:",
+                                        key="matchid")   
+            with rsid02:
+                Matchday = st.text_input("Matchday:",
+                                         key="matchday")   
+
+            rsid03, rsid04 = st.columns(2)
+            with rsid03:
+                CompetitionID = st.text_input("CompetitionID:",
+                                              key="competitionid")   
+            with rsid04:
+                SeasonID = st.text_input("SeasonID:",
+                                         key="seasonid")
+   
+            submit_button = st.form_submit_button(label='Aceptar')  
+     
+            
+    #df = pd.read_excel(Table)
+
+    st.markdown("""---""")
+    st.markdown("<style> div { text-align: center } </style>", unsafe_allow_html=True)
+    st.title("EVENT DATA")
