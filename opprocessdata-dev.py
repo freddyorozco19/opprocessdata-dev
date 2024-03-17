96b9ae71a52878a8497137065f0b7f8766986bf2
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-03-17 03:19:42 -0500

    Update opprocessdata-dev.py

--- opprocessdata-dev.py
+++ opprocessdata-dev.py
@@ -341,7 +341,7 @@ if menu_id == "ExploreTeamData":
                 ax9.set_xlim(0,10)
                 ax9.set_ylim(0,10)
                 ax9.scatter(2, 5, s=120, color=colorviz, edgecolors='#FFFFFF', lw=1)
-                ax9.text(2, 0.75, 'ACCIONES', fontproperties=prop2, fontsize=9, ha='center', va='center', c='w')
+                ax9.text(2, 1.0, 'ACCIONES', fontproperties=prop2, fontsize=9, ha='center', va='center', c='w')
 
                 st.pyplot(fig, bbox_inches="tight", pad_inches=0.05, dpi=400, format="png")
             if OptionPlotSel == 'Territory Actions': 
